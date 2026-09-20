bcbc33736deeece7522569f40f6b49f57e3c255e
Ilya <NoodleFall@proton.me>
2026-09-21 03:07:12 +0500

    7: groups

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ from player import Player
 def main():
     pygame.init()
 
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x, y)
+
     while True:
         log_state()
 
@@ -21,10 +26,13 @@ def main():
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
+        print(len(updatable))
         print(dt)
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
